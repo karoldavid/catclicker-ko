c8a55d526fce1947c98bfe2569aa3c02a6c99f18
lorak <k.zysk@zoho.com>
2015-06-20 17:39:48 +0200

    add cat data

---
+++ js/data.js
@@ -0,0 +1,77 @@
+      var cats = {
+            "kittens" : [
+              {
+                "name" : "Ninni",
+                "imgSrc"  : "kitten-1.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Ninno"]
+              },
+              {
+                "name" : "Hugo",
+                "imgSrc"  : "kitten-2.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Higi"]
+              },
+              {
+                "name" : "Nora",
+                "imgSrc"  : "kitten-3.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Nirri"]
+              },
+              {
+                "name" : "Ridder",
+                "imgSrc"  : "kitten-4.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Rilll"]
+              },
+              {
+                "name" : "Waldo",
+                "imgSrc"  : "kitten-5.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Wiwi"] 
+              },
+              {
+                "name" : "Mux",
+                "imgSrc"  : "kitten-6.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Mixulian"]
+              },
+              {
+                "name" : "Barollo",
+                "imgSrc"  : "kitten-7.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Bababllo"]
+              },
+              {
+                "name" : "Bezzi",
+                "imgSrc"  : "kitten-8.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Bezzita"]
+              },
+              {
+                "name" : "Nemki",
+                "imgSrc"  : "kitten-9.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Niminnini"]
+              },
+              {
+                "name" : "Daduda",
+                "imgSrc"  : "kitten-10.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Dadadu"]
+              },
+              {
+                "name" : "Gerdi",
+                "imgSrc"  : "kitten-11.jpg",
+                "clickCount" : 0,
+                "nicknames" : ["Gigi"]
+              },
+              {
+                "name" : "Josi",
+                "imgSrc"  : "kitten-12.jpg",
+                "clickCount": 0,
+                "nicknames" : ["Jojo"]
+
+              }
+           ]
+         };
