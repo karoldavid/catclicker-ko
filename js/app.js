5599f51993e1c4d192765a2286b569f0c0a89a53
lorak <k.zysk@zoho.com>
2015-06-20 17:02:46 +0200

    change binding context of ViewModel to self

--- js/app.js
+++ js/app.js
@@ -31,10 +31,11 @@ var Cat = function() {
 };
 
 var ViewModel = function() {
+	var self = this;
     this.currentCat = ko.observable(new Cat());
 
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};	
 };
 
